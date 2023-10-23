5e476d1f6b8737fe5a7fc799404ae729db198adf
Karla Jaime <kyjhgg@umsystem.edu>
2023-10-23 15:20:43 -0500

    Added webservice.py

---
+++ webservice.py
@@ -0,0 +1,7 @@
+# This is mostly to refresh on Git commands, but it could also be a starting point.
+
+def main():
+
+    print("How about here we print out all the text, and then run the functions?")
+
+main()
