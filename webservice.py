7cb3732a025c1f37b1b541cbe15e9f1b2bbe64d1
Karla Jaime <kyjhgg@umsystem.edu>
2023-10-25 18:05:05 -0500

    Trying to push this branch to GitHub

--- webservice.py
+++ webservice.py
@@ -11,13 +11,13 @@ def jprint(obj):
 def get_time_series(number):
 
     if number == 1:
-        return 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=60min&apikey=9I22O100RNSZ6IPR'
+        return ['INTRADAY', '60min']
     elif number == 2:
-        return 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=9I22O100RNSZ6IPR'
+        return ['DAILY', 'null']
     elif number == 3:
-        return 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=9I22O100RNSZ6IPR'
+        return ['WEEKLY', 'null']
     else:
-        return 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=IBM&apikey=9I22O100RNSZ6IPR'
+        return ['MONTHLY', 'null']
 
 def main():
 
@@ -30,7 +30,9 @@ def main():
             print("1. Intraday\n2. Daily\n3. Weekly\n4. Monthly\n")
             answer = int(input("Enter the time series option (1, 2, 3, 4): "))
 
-            response = requests.get(get_time_series(answer))
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_{}&symbol=IBM&interval={}&apikey=9I22O100RNSZ6IPR'.format(get_time_series(answer))
+
+            response = requests.get(url)
             jprint(response.json())
 
             run_again = input("Would you like to view more stock data? Press 'y' to continue: ")
