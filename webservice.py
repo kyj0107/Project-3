fad826da45c36a0fd6597396342bd07c8c780e35
EldonJH <101818396+EldonJH@users.noreply.github.com>
2023-10-26 15:17:12 -0500

    Update webservice.py
    Add input for star and end date, add validation for dates

--- webservice.py
+++ webservice.py
@@ -1,4 +1,23 @@
 # This is mostly to refresh on Git commands, but it could also be a starting point.
+import requests
+import json
+import pygal
+from datetime import datetime
+import webbrowser
+
+start_date = input("Enter the beginning date in YYYY-MM-DD format: ")
+end_date = input("Enter the end date in YYYY-MM-DD format: ")
+# Validate the dates
+try:
+    start_date_obj = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date_obj = datetime.strptime(end_date, '%Y-%m-%d')
+except ValueError:
+    print("Invalid date format. Please use YYYY-MM-DD.")
+    exit()
+
+if end_date_obj < start_date_obj:
+    print("End date cannot be before the start date.")
+    exit()
 
 def get_chart():
     while True:
